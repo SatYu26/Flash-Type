14c7b11f41fc946a0d4aa5a0ecbc2d7dcdc200cf
SatYu26 <goyalsatyam8@gmail.com>
2021-04-03 02:31:37 +0530

    Building the Navigation Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Nav from "./../Nav/Nav";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>hi</h1>
+        <Nav />
       </div>
     );
   }
