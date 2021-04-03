ab33455a1329057a81f69af2cf51b3570e0920e8
SatYu26 <goyalsatyam8@gmail.com>
2021-04-04 00:35:45 +0530

    Building the Footer Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Nav from "./../Nav/Nav";
 import Landing from "../Landing/Landing";
+import Footer from "../Footer/Footer";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="app">
         <Nav />
         <Landing />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <a
+        target="_blank"
+        className="footer-link"
+        href="https://satyamgoyal.codes"
+        rel="noreferrer"
+      >
+        Say Hi.!
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
